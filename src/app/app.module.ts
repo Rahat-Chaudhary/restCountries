c9d204f59031ff35788277ea4d1c2f2151a15f6d
Rahat Siddiq <Rahat Siddiq>
2019-08-07 10:09:43 +0200

    routes are /countires and /country/:name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,48 @@
-import { CountryDetailComponent } from './countryDetail/countryDetail.component';
-import { CountriesService } from './services/countries.service';
-import { NotFoundComponent } from './not-found/not-found.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { MatCardModule } from '@angular/material/card';
+import { CountryDetailComponent } from "./countryDetail/countryDetail.component";
+import { CountriesService } from "./services/countries.service";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { MatCardModule } from "@angular/material/card";
 
-import { AppComponent } from './app.component';
-import { CountriesComponent } from './countries/countries.component';
+import { AppComponent } from "./app.component";
+import { CountriesComponent } from "./countries/countries.component";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      CountriesComponent,
-      NotFoundComponent,
-      CountryDetailComponent
-   ],
-   imports: [
-      BrowserModule,
-      BrowserAnimationsModule,
-      HttpClientModule,
-      MatCardModule,
-      RouterModule.forRoot([
-        {
-          path: '' , 
-          component : CountriesComponent
-        },
-        {
-          path: 'country/:name' , 
-          component : CountryDetailComponent
-        },
-        {
-          path: '**' , component : NotFoundComponent
-        },
-        
-      ])
-   ],
-   providers: [CountriesService],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    CountriesComponent,
+    NotFoundComponent,
+    CountryDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
+    RouterModule.forRoot([
+      {
+        path: "",
+        component: CountriesComponent
+      },
+      {
+        path: "countries",
+        component: CountriesComponent
+      },
+      {
+        path: "country/:name",
+        component: CountryDetailComponent
+      },
+      {
+        path: "**",
+        component: NotFoundComponent
+      }
+    ])
+  ],
+  providers: [CountriesService],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
